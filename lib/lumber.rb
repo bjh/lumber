5c9d79fee4d16c55ba12fbb5e06434773ac12e0b
brodie hanbuch <brodie.hanbuch@gmail.com>
2011-02-08 21:47:46 -0500

    branchy branch

--- lib/lumber.rb
+++ lib/lumber.rb
@@ -1,69 +1,18 @@
-require 'logger'
+puts "PATH: #{$:}"
 
-begin
-  require 'ansi'
-  $ansi_included = true
-rescue LoadError => error
-  puts "run 'gem install ssoroka-ansi' to get colored log output"
-  $ansi_included = false
-end
+require 'logger'
+require 'ansi'
+require 'lumber/colored_output'
+
 
 
 # small wrapper around Ruby's Logger class
-# to get colored output and simple messages
-module Lumber
-  # handles coloring Logger output if available
-  class ColoredOutput < Logger::Formatter    
-    # intercept calls to Logger
-    def call(severity, time, program_name, message)
-      severity = "_#{severity.downcase}".to_sym      
-      begin
-        self.send(severity, message)
-      rescue
-        super
-      end
-    end    
-  private  
-    def _format(message)
-      msg = colon() + message      
-      #[border(), msg, border()].join("\n") + "\n"
-      "#{msg}\n"
-    end
-
-    def _error(message)
-      _format(ANSI.color(:red, :bold => true) { "#{message}" })
-    end
-
-    def _info(message)
-      _format(ANSI.color(:yellow, :bold => true) { "#{message}" })
-    end
-
-    def _warn(message)
-      _format(ANSI.color(:magenta, :bold => true) { "#{message}" })
-    end
-
-    def _debug(message)
-      _format(ANSI.color(:cyan, :bold => true) { "#{message}" })
-    end
-
-    def colon
-      ANSI.color(:black, :bold => true) {": "}
-    end
-
-    def border(size=1)
-      "-" * size
-    end  
-  end
-  
-  #------------------------------------------------
-  
+module Lumber  
   # module level variable
   @logger = Logger.new($stdout)
   @logger.datetime_format = "%H:%M "
   
-  if $ansi_included
-    @logger.formatter = ColoredOutput.new
-  end
+  @logger.formatter = Lumber::ColoredOutput.new
 
   def self.date_time_format(format)
     @logger.datetime_format = format
