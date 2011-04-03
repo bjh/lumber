b0bf7955d2844697e4cefb037114fa15437df853
brodie hanbuch <brodie.hanbuch@gmail.com>
2011-04-03 10:20:57 -0400

    shazam

--- lib/lumber.rb
+++ lib/lumber.rb
@@ -8,57 +8,19 @@ require 'lumber/colored_output'
 module Lumber  
   CONFIG = Lumber::Config.new
   
-  @logger = Logger.new($stdout)
-  @logger.datetime_format = "%H:%M "
+  @@logger ||= Logger.new($stdout)
+  @@logger.datetime_format = "%H:%M "
   
-  @logger.formatter = Lumber::ColoredOutput.new
-
-  # attach methods now instead of using method_missing
-  # methods = %w{datetime_format level error info warn debug}
-
-  
-  # class << self
-  #   methods.each do |method|p
-  #     define_method(method) do |*args|
-  #       @logger.send(method, *args)
-  #     end
-  #   end
-  # end
+  @@logger.formatter = Lumber::ColoredOutput.new
 
   class << self
     def method_missing(method, *args, &block)
-      if @logger.respond_to?(method)
-        @logger.send(method, *args)
+      if @@logger.respond_to?(method)
+        @@logger.send(method, *args)
       else
-        @logger.warn "method_missing - logger does not respond to #{method}"
+        @@logger.warn "method_missing - logger does not respond to #{method}"
       end
     end
   end
-
-  # def self.date_time_format(format)
-  #   @logger.datetime_format = format
-  # end
-  
-  # def level(log_level)
-  #   @logger.level = log_level
-  # end
-  
-  # def self.error(message)
-  #   @logger.error(message)
-  # end
-  
-  # def self.info(message)
-  #   @logger.info(message)
-  # end
-
-  # def self.warn(message)
-  #   @logger.warn(message)
-  # end
-  
-  # def self.debug(message)
-  #   @logger.debug(message)
-  # end
 end
-
-
 
